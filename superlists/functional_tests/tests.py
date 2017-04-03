d9de5c61cf97280145ed745d456f52d2fea0d07d
daniel <elastic7327@gmail.com>
2017-04-03 20:48:02 +0800

    Hoooorayyy

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -27,7 +27,7 @@ class NewVisitorText(LiveServerTestCase):
 
     def wait_for_row_in_list_table(self, row_text):
 
-        start_item = time.time()
+        start_time = time.time()
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
@@ -38,6 +38,53 @@ class NewVisitorText(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith start a new todo list
+
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+')
+
+        # Now a new user, Francis, comes along to the site.
+
+        # We use a new browser session to make sure that no information
+        # of Edith's is coming through from cookies etc
+
+        self.browser.quit()
+        self.browser.webdriver.Chrome()
+
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by enterning a new item. He
+        # is less interesting than Edith . . .
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.asserttIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes

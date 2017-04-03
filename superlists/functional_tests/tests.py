36c8362846ac3f469f591f6a1b6c12d895eedcc9
daniel <elastic7327@gmail.com>
2017-04-03 20:22:51 +0800

    Hoooorray

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 import unittest
+
+MAX_WAIT = 10
 
 
 class NewVisitorText(LiveServerTestCase):
@@ -21,6 +24,20 @@ class NewVisitorText(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+
+        start_item = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -52,7 +69,7 @@ class NewVisitorText(LiveServerTestCase):
         # "1: Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
 
@@ -66,9 +83,8 @@ class NewVisitorText(LiveServerTestCase):
 
         # The page updates again, and now shows both items on her list
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then
         # She sees

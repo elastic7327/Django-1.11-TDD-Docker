5dd7f33478b26f106423bdc4b738c6700681354f
daniel <elastic7327@gmail.com>
2017-04-03 17:17:56 +0800

    View and Model up

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -23,6 +23,10 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
         self.assertIn('A new list item', response.content.decode('utf8'))
         self.assertTemplateUsed(response, 'lists/home.html')
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from lists.models import Item
 
 
 def home_page(request):
-    return render(request, 'lists/home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
-
-
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()

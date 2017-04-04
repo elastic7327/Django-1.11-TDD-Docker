9092c1fcc8c9e2e2be111ce3978aae9e87d20b2a
daniel <elastic7327@gmail.com>
2017-04-04 22:32:47 +0800

    TDD

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -6,7 +6,9 @@
 import unittest
 import time
 
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,7 +17,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 2
 
 
-class NewVisitorText(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """
     Something Special Doc String
     """
@@ -50,6 +52,34 @@ class NewVisitorText(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+    def test_layout_and_styling(self):
+        """
+        DocString
+        """
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
+        )
+
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         """

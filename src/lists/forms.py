c13d336aaa3eae53205ac05a663cbc302506a001
daniel <elastic7327@gmail.com>
2017-04-05 15:50:57 +0800

    Forms RED -> GReen light

---
+++ src/lists/forms.py
@@ -0,0 +1,39 @@
+"""
+    DOC STRING
+"""
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django import forms
+from lists.models import Item
+
+
+# class ItemForm(forms.Form):
+#     """
+#     DOC  DOC  DOC
+#     """
+#     item_text = forms.CharField(
+#             widget=forms.fields.TextInput(attrs={
+#                 'placeholder': 'Enter a to-do item',
+#                 'class': 'form-control input-lg',
+#         }),
+#     )
+
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+class ItemForm(forms.models.ModelForm):
+    """
+    DOC  DOC  DOC
+    """
+    class Meta:
+        model = Item
+        fields = ('text', )
+        widgets = {
+                'text': forms.fields.TextInput(attrs={
+                    'placeholder': 'Enter a to-do item',
+                    'class' : 'form-control input-lg',
+                }),
+        }
+        error_messages = {
+                'text': {'required': "You can't have an empty list item"}
+        }

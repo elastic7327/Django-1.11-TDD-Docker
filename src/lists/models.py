6a0dcdd15ec84dabf9f80a8e3b3d618dbc6a647b
daniel <elastic7327@gmail.com>
2017-04-05 18:49:48 +0800

    django ordering and compareing objects

--- src/lists/models.py
+++ src/lists/models.py
@@ -18,6 +18,12 @@ class Item(models.Model):
     """
     DOC STRING
     """
-
-    text = models.TextField(default='')
+    text = models.TextField(default='', unique=True)
     list = models.ForeignKey(List, default=None)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')

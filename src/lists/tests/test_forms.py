895dfa58cdca2225b1ff10f64d7a7d73254b4e14
daniel <elastic7327@gmail.com>
2017-04-05 17:47:36 +0800

    I so much hate typo.....error

--- src/lists/tests/test_forms.py
+++ src/lists/tests/test_forms.py
@@ -8,6 +8,8 @@ from django.test import TestCase
 from django.utils.html import escape
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
+from lists.models import List, Item
+
 import ipdb as br
 
 
@@ -22,8 +24,15 @@ class ItemFormTest(TestCase):
 
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
-        print(form.as_p())
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['text'], [EMPTY_ITEM_ERROR]
         )
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
--- src/lists/views.py
+++ src/lists/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from lists.models import Item, List
 from lists.forms import ItemForm
 
+import ipdb as br
+
 
 def home_page(request):
     return render(request, 'lists/home.html', {'form': ItemForm()})
@@ -16,7 +18,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'lists/list.html', {'list': list_, 'form': form })
 
@@ -25,7 +27,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/home.html', {'form': form})
